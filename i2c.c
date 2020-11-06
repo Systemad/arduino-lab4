abc32adc0843d768931ff5e9d3ac9dec43648f1c
systemad <dannepuke@gmail.com>
2020-11-06 20:49:16 +0100

    Labb4 Assignment 1 and 2
    First assignment we make functions to control an external EEPROM, this is done by sending control bit with READ or WRITE bit (see idc.c) and the address in EEPROM where we want to store in.
    
    Assignment 2 takes advantage of these functions in order to write bytes to (multiple) addressess and reading from said addresses.
    Check comments in idc.c for more information,
    
    In main.c is a demostration how its done.

--- i2c.c
+++ i2c.c
@@ -7,8 +7,16 @@
 
 #include "i2c.h"
 
-void i2c_init(void) {
-	// ...
+#define F_CPU 16000000UL
+#define frequency 100000 // 100 kHz as prescaler
+
+// This is the address EEPROM, first 4 bits are control bits, 3 after are irrelevant and last one is READ(0) or WRITE(1) 
+#define eepromAdr 0b10100000
+
+void i2c_init(void) //Set the prescaler to 1 and writes our TWBR-value so we get our desired SCL_FREQUENCY.
+{
+	TWCR &= ~(1 << TWPS1) | ~(1 << TWPS0);
+	TWBR = ((F_CPU / frequency - 16) / 2);
 }
 
 void i2c_meaningful_status(uint8_t status) {
@@ -55,43 +63,86 @@ void i2c_meaningful_status(uint8_t status) {
 }
 
 inline void i2c_start() {
-	// ...
+
+	// Send start condition
+	TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);
+	while (!(TWCR & (1 << TWINT)));
 }
 
 inline void i2c_stop() {
-	// ...
+	// Send stop condition
+	TWCR = (1 << TWINT)  | (1 << TWSTO) | (1 << TWEN);
+	
+	while ((TWCR & (1 << TWSTO)));
 }
 
 inline uint8_t i2c_get_status(void) {
-	// ...
+
+	// Send TWSR to i2c_meaningful_status to print readable status
+	i2c_meaningful_status(TWSR);
 }
 
 inline void i2c_xmit_addr(uint8_t address, uint8_t rw) {
-	// ...
+
+	// Load address to register
+	TWDR = address | rw;
+	TWCR = (1 << TWINT) | (1 << TWEN);
+	while (!(TWCR & (1 << TWINT))); // == 0 not working
 }
 
 inline void i2c_xmit_byte(uint8_t data) {
-	// ...
+	
+	// Load data to register
+	TWDR = data;
+	TWCR = (1 << TWINT) | (1 << TWEN);
+	while (!(TWCR & (1 << TWINT)));
 }
 
 inline uint8_t i2c_read_ACK() {
-	// ...
+	TWCR = (1 << TWINT) | (1 << TWEA) | (1<<TWEN); // Send acknowledge bit
+	while (!(TWCR & (1 << TWINT)));
+	return TWDR;
 }
 
 inline uint8_t i2c_read_NAK() {
-	// ...
+	TWCR = (1 << TWINT) | (1<<TWEN);
+	while (!(TWCR & (1 << TWINT)));
+	return TWDR;
 }
 
 inline void eeprom_wait_until_write_complete() {
-	// ...
+	// Acknowledge polling, loop to check if if write is complete
+	for(;;){
+		i2c_start();
+		i2c_xmit_addr(eepromAdr, I2C_W);
+		if(TWSR == 0x18) break; // break loop if transmitted and acknowledged
+	}
 }
 
+
 uint8_t eeprom_read_byte(uint8_t addr) {
-	// ...
+	// Inital reading byte by starting and sending eeprom address and where in eeprom where we want read from
+	i2c_start();
+	i2c_xmit_addr(eepromAdr, I2C_W); // send write bit
+	i2c_xmit_byte(addr);
+	
+	i2c_start();
+	i2c_xmit_addr(eepromAdr, I2C_R); // Send read bit
+	
+	// Store to read variable and return it
+	uint8_t read = i2c_read_NAK();
+	i2c_stop(); // stop i2C
+	return read;
 }
 
 void eeprom_write_byte(uint8_t addr, uint8_t data) {
-	// ...
+	
+	// Inital reading byte by starting i2c and sending eeprom address and where in eeprom where we want to store
+	i2c_start();
+	i2c_xmit_addr(eepromAdr, I2C_W);
+	i2c_xmit_byte(addr);
+	i2c_xmit_byte(data);
+	i2c_stop();
 }
 
 
--- main.c
+++ main.c
@@ -15,12 +15,32 @@
 void main (void) {
 
 	i2c_init();
-	uart_init();
+	uart_init();	
 
 	sei();
 
 	while (1) {
 
+		eeprom_write_byte(0x10, 'd');
+		eeprom_wait_until_write_complete();
+
+		eeprom_write_byte(0x11, 'a');
+		eeprom_wait_until_write_complete();
+
+		eeprom_write_byte(0x12, 'n');
+		eeprom_wait_until_write_complete();
+
+		char ByteOfData = eeprom_read_byte(0x10);
+		printf("%c", ByteOfData);
+
+		char ByteOfData2 = eeprom_read_byte(0x11);
+		printf("%c", ByteOfData2);
+
+		char ByteOfData3 = eeprom_read_byte(0x12);
+		printf("%c", ByteOfData3);
+		printf("\n");
+		
+		_delay_ms(1000);
+
 	}
 }
-
